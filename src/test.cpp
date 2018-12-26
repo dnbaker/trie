ed5d79e807d618d160d1f61278a261caada4d769
dnbh <dnb@cs.jhu.edu>
2018-12-26 12:05:52 -0500

    Parses, sorts, and inserts all sequences from a .{fa,fna,fasta,fq,fastq}{.gz}?

--- src/test.cpp
+++ src/test.cpp
@@ -2,8 +2,10 @@
 #include "SuRF/include/surf.hpp"
 
 int main(int argc, char *argv[]) {
-    {
-       fx::FxFile ff(argc == 1 ? "z2.fa": (const char *)argv[1]);
-       surf::SuRF(ff.sbegin(), ff.send());
-    }
+    fx::FxFile ff(argc == 1 ? "z2.fa": (const char *)argv[1]);
+    std::vector<std::string> strs;
+    for(const auto &s: ff) 
+        strs.push_back(s.s_);
+    std::sort(strs.begin(), strs.end());
+    surf::SuRF sup(strs);
 }
